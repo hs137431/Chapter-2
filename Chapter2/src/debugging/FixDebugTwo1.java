ca97f5c1c89c6d3769758c96808a0b5ce79b296e
hs137431 <31348463+hs137431@users.noreply.github.com>
2017-09-11 09:26:19 -0500

    Completed Debugging

---
+++ Chapter2/src/debugging/FixDebugTwo1.java
@@ -0,0 +1,16 @@
+package debugging;
+
+public class FixDebugTwo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int oneInt = 315;
+		double oneDouble = 12.4;
+		char oneChar = 'A';
+		System.out.println("The int is " + oneInt);
+	    System.out.println("The double is " + oneDouble);
+	    System.out.println("The char is " + oneChar);
+	    
+	}
+
+}
---
+++ Chapter2/src/debugging/FixDebugTwo2.java
@@ -0,0 +1,24 @@
+package debugging;
+
+public class FixDebugTwo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a, b;
+		 a = 7;
+	      b = 4;
+	      System.out.println("The first number is " + a + " the second number is " + b);
+	      System.out.println("The sum is " + (a + b));
+	      System.out.println("The difference is " + (a - b));
+	      System.out.println("The product is " + (a * b));
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+
+}
